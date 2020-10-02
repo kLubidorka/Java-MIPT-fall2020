20ab33fe5290169157fc45e8620911fd8b16f117
Ivan <39248996+kLubidorka@users.noreply.github.com>
2020-10-02 19:18:08 +0300

    added tasks

--- Sem5/RegEx/src/main/java/RegExExample.java
+++ Sem5/RegEx/src/main/java/RegExExample.java
@@ -11,4 +11,27 @@ public class RegExExample {
         boolean b = m.matches();
         System.out.println(b);
     }
+
+    /**
+     * Validates a sentence. Checks if first letter is capital and it ends with dot, question mark or exclamation mark.
+     * @param input String with the sentence to check
+     * @return true if sentence is correct, false otherwise
+     */
+    public static boolean validateInput(String input){
+        Pattern p = Pattern.compile("[A-Z].*[/.?!]");
+        Matcher m = p.matcher(input);
+        return m.matches();
+    }
+
+    /**
+     * Validates email address. Email should contain one at-symbol (@) and at least one symbol to the left and to the right
+     * from at-symbol. It can contain letters, numbers and symbols '.', '-', '_'
+     * @param email String with email to validate
+     * @return true if email is correct, false otherwise
+     */
+    public static boolean validateEmail(String email){
+        Pattern p = Pattern.compile("YOUR PATTERN HERE");
+        Matcher m = p.matcher(email);
+        return m.matches();
+    }
 }
--- Sem5/RegEx/src/test/java/InputTest.java
+++ Sem5/RegEx/src/test/java/InputTest.java
@@ -1,3 +1,28 @@
-public class InputTest {
+import static org.junit.jupiter.api.Assertions.*;
 
+public class InputTest {
+    @org.junit.jupiter.api.Test
+    public void ExampleTest(){
+        assertTrue(RegExExample.validateInput("Hello."));
+        assertTrue(RegExExample.validateInput("Hello!"));
+        assertTrue(RegExExample.validateInput("Hello?"));
+
+        assertFalse(RegExExample.validateInput("Hello"));
+        assertFalse(RegExExample.validateInput("hello"));
+        assertFalse(RegExExample.validateInput("hello."));
+    }
+
+    @org.junit.jupiter.api.Test
+    public void EmailTest(){
+        assertTrue(RegExExample.validateEmail("abc@gmail.com"));
+        assertTrue(RegExExample.validateEmail("a_b_c@gmail.com"));
+        assertTrue(RegExExample.validateEmail("A.bc@gmail.com"));
+
+        assertFalse(RegExExample.validateEmail("abc@"));
+        assertFalse(RegExExample.validateEmail("@gmail.com"));
+        assertFalse(RegExExample.validateEmail("a*bc@gmail.com"));
+        assertFalse(RegExExample.validateEmail("a$bc@gmail.com"));
+        assertFalse(RegExExample.validateEmail("a@bc@gmail.com"));
+        assertFalse(RegExExample.validateEmail("a*bc@@gmail.com"));
+    }
 }
