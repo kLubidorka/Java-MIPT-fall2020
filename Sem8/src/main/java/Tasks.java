5858f8ddd5bacb7865af011ade1ac2e2f9e0a703
Ivan <39248996+kLubidorka@users.noreply.github.com>
2020-10-23 03:39:54 +0400

    added task 2

--- Sem8/src/main/java/Tasks.java
+++ Sem8/src/main/java/Tasks.java
@@ -1,7 +1,10 @@
+import java.util.Collection;
 import java.util.stream.Stream;
 
+import static java.lang.Math.max;
+
 /**
- * This class contains tasks that should be solved using stream API.
+ * This class contains tasks that should be solved using stream API only.
  * Check your solutions with TaskTester (in src/main/test/java/ folder)
  */
 public class Tasks<T> {
@@ -13,5 +16,13 @@ public class Tasks<T> {
     public long task1(Stream<T> stream, T pattern){
         // YOUR CODE HERE
         return 0;
+    }
+
+    /**
+     * Return last element of collection or null if collection os empty
+     */
+    public T task2(Collection<T> collection){
+        // YOUR CODE HERE
+        return null;
     }
 }
--- Sem8/src/test/java/TasksTester.java
+++ Sem8/src/test/java/TasksTester.java
@@ -1,6 +1,12 @@
 import org.junit.jupiter.api.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class TasksTester {
     @Test
@@ -14,5 +20,12 @@ public class TasksTester {
         assertEquals(intTask.task1(Stream.of(1, 2, 3), 2), 1);
         assertEquals(intTask.task1(Stream.of(1, 2, 3, 2, 2), 2), 3);
         assertEquals(intTask.task1(Stream.of(1, 2, 3), 4), 0);
+    }
+
+    @Test
+    void task2(){
+        Tasks<Double> doubleTask = new Tasks<>();
+        assertEquals(doubleTask.task2(Arrays.asList(1D, 2D, 3D, 15D)), 15);
+        assertNull(doubleTask.task2(Collections.emptyList()));
     }
 }
