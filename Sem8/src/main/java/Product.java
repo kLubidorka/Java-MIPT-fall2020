eb1ae80ac8bf93a503c6a14ec42aa757be8c3e06
Ivan <39248996+kLubidorka@users.noreply.github.com>
2020-10-22 23:57:42 +0400

    added example

---
+++ Sem8/src/main/java/Product.java
@@ -0,0 +1,33 @@
+public class Product implements Comparable<Product> {
+    private int id;
+    private int price;
+    private String name;
+
+    Product(int id, int price, String name) {
+        this.id = id;
+        this.price = price;
+        this.name = name;
+    }
+
+    int getId() {
+        return id;
+    }
+
+    int getPrice() {
+        return price;
+    }
+
+    String getName() {
+        return name;
+    }
+
+    @Override
+    public int compareTo(Product product) {
+        return Integer.compare(this.id, product.id);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Product %s with id %d costs %d standard units", name, id, price);
+    }
+}
--- Sem8/src/main/java/StreamExamples.java
+++ Sem8/src/main/java/StreamExamples.java
@@ -1,5 +1,57 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class StreamExamples {
     public static void main(String[] args) {
+        beforeJava8Approach();
+    }
 
+    /**
+     * Our goal is to find all the products that are cheaper than 14 standard units, sort them by id and print them.
+     * This method solves the problem without streams
+     */
+    public static void beforeJava8Approach(){
+        List<Product> products = prepareProducts();
+
+        // Find all the products that are cheaper than 14 standard units
+        List<Product> cheapProducts = new ArrayList<>();
+        for (Product elem : products){
+            if (elem.getPrice() < 14){
+                cheapProducts.add(elem);
+            }
+        }
+
+        // Sort them
+        Collections.sort(cheapProducts);
+
+        // Print them
+        for (Product elem : cheapProducts){
+            System.out.println(elem);
+        }
+    }
+
+    /**
+     * This method solves the problem using stream
+     */
+    public static void streamApproach(){
+        List<Product> products = prepareProducts();
+
+        products.stream()
+                .filter(s -> s.getPrice() < 14)
+                .sorted()
+                .forEach(System.out::println);
+    }
+
+
+    private static List<Product> prepareProducts(){
+        List<Product> products = new ArrayList<>(5);
+        products.add(new Product(1, 20, "Banana"));
+        products.add(new Product(2, 10, "Candy"));
+        products.add(new Product(3, 12, "Juice"));
+        products.add(new Product(4, 15, "Apple"));
+
+        return products;
     }
 }
