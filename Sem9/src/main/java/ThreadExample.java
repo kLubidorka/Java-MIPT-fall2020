8ef89e8af6a78b10069e5f15507ecef2a77ed9ea
Ivan <39248996+kLubidorka@users.noreply.github.com>
2020-10-30 01:52:19 +0400

    added examples

--- Sem9/src/main/java/ThreadExample.java
+++ Sem9/src/main/java/ThreadExample.java
@@ -1,5 +1,37 @@
 public class ThreadExample {
     public static void main(String[] args) {
+        // #1
+        Thread thread1 = new Thread(new HelloRunnable());
+        thread1.start();
 
+        // #2
+        HelloThread thread2 = new HelloThread(2);
+        thread2.start();
+
+        reordering();
+    }
+
+    public static void reordering(){
+        for (int i = 0; i < 10; i++){
+            new HelloThread(i).start();
+        }
+    }
+}
+
+class HelloRunnable implements Runnable {
+    public void run() {
+        System.out.println("Hello from thread #1");
+    }
+}
+
+class HelloThread extends Thread {
+    int idx;
+
+    HelloThread(int num){
+        idx = num;
+    }
+
+    public void run() {
+        System.out.printf("Hello from thread #%d\n", idx);
     }
 }
