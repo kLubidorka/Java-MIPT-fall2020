729f2038e18945e2d47923accda8a6b91caa65e3
Ivan <39248996+kLubidorka@users.noreply.github.com>
2020-11-04 23:46:20 +0400

    added one more example

--- Sem10/src/main/java/LowLevelConcurrencyPrimitivesDemo.java
+++ Sem10/src/main/java/LowLevelConcurrencyPrimitivesDemo.java
@@ -1,3 +1,4 @@
+import java.util.List;
 import java.util.stream.Stream;
 
 public class LowLevelConcurrencyPrimitivesDemo {
@@ -15,9 +16,9 @@ public class LowLevelConcurrencyPrimitivesDemo {
 
     public static void raceConditionFixed() {
         // NO RACE CONDITION
-        IncrementSynch inc1 = new IncrementSynch();
-        Stream.iterate(0, n -> n + 1).limit(10_000).parallel().forEach(n -> inc1.increment());
-        System.out.println(inc1.value);
+        IncrementSynch inc2 = new IncrementSynch();
+        Stream.iterate(0, n -> n + 1).limit(10_000).parallel().forEach(n -> inc2.increment());
+        System.out.println(inc2.value);
     }
 
 }
@@ -34,9 +35,52 @@ class Increment {
  * Does not cause concurrent errors, since method is synchronized
  */
 class IncrementSynch {
+
+    /** Constructors cannot be synchronized. Using the synchronized keyword with a constructor is a syntax error.
+     * Synchronizing constructors doesn't make sense, because only the thread that creates an object
+     * should have access to it while it is being constructed.
+     */
+//    synchronized IncrementSynch(){
+//        // do smth
+//    }
+
     int value = 0;
 
     public synchronized void increment() {
         value++;
+    }
+}
+
+class DataTransferObject{
+    List<Object> firstList;
+    List<Object> secondList;
+
+    public synchronized void setFirstList(List<Object> list){
+        firstList = list;
+    }
+
+    public synchronized void setSecondList(List<Object> list){
+        secondList = list;
+    }
+}
+
+class DataTransferObjectImproved{
+    List<Object> firstList;
+    final Object firstListLock = new Object();
+
+    List<Object> secondList;
+    final Object secondListLock = new Object();
+
+    public void setFirstList(List<Object> list){
+        synchronized (firstListLock){
+            firstList = list;
+        }
+
+    }
+
+    public void setSecondList(List<Object> list){
+        synchronized (secondListLock){
+            secondList = list;
+        }
     }
 }
