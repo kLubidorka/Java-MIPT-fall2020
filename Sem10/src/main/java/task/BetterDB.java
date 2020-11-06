4e7241e38738861d763ca0b1ef9eff1a591b67d4
Ivan <39248996+kLubidorka@users.noreply.github.com>
2020-11-06 12:59:46 +0400

    added task

---
+++ Sem10/src/main/java/task/BetterDB.java
@@ -0,0 +1,16 @@
+package task;
+
+public class BetterDB implements DB {
+    long READ_TIME = 500; // milliseconds
+    long WRITE_TIME = 1000; // milliseconds
+
+    @Override
+    public void read(int readerID) {
+
+    }
+
+    @Override
+    public void write(int writerId) {
+
+    }
+}
---
+++ Sem10/src/main/java/task/PrimitiveDB.java
@@ -0,0 +1,14 @@
+package task;
+
+public class PrimitiveDB implements DB {
+    long READ_TIME = 500; // milliseconds
+    long WRITE_TIME = 1000; // milliseconds
+
+    public void read(int readerID){
+
+    }
+
+    public void write(int writerId){
+
+    }
+}
