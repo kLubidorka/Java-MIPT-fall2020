754fa2b92515a4a4c4f818387f319fce8b7414a7
Ivan <39248996+kLubidorka@users.noreply.github.com>
2020-11-13 02:08:23 +0400

    added sem11

---
+++ Sem11/src/main/java/AtomicExamples.java
@@ -0,0 +1,51 @@
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
+
+public class AtomicExamples {
+    public static void main(String[] args) {
+
+    }
+
+    public static void raceCondition() {
+        // RACE CONDITION
+        Increment inc1 = new Increment();
+        Stream.iterate(0, n -> n + 1).limit(10_000).parallel().forEach(n -> inc1.increment());
+        System.out.println(inc1.value());
+    }
+
+    public static void noRaceCondition(){
+        // RACE CONDITION
+        AtomicCounter  inc2 = new AtomicCounter ();
+        Stream.iterate(0, n -> n + 1).limit(10_000).parallel().forEach(n -> inc2.increment());
+        System.out.println(inc2.value());
+    }
+}
+
+class Increment {
+    private int value = 0;
+
+    public void increment() {
+        value++;
+    }
+
+    public int value(){
+        return value;
+    }
+}
+
+class AtomicCounter {
+    private AtomicInteger c = new AtomicInteger(0);
+
+    public void increment() {
+        c.incrementAndGet();
+    }
+
+    public void decrement() {
+        c.decrementAndGet();
+    }
+
+    public int value() {
+        return c.get();
+    }
+
+}
