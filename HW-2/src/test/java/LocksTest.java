cec82d445618677c3e5689100d54d8547bf829de
Ivan <39248996+kLubidorka@users.noreply.github.com>
2020-11-21 23:45:23 +0400

    upd tests

--- HW-2/src/test/java/LocksTest.java
+++ HW-2/src/test/java/LocksTest.java
@@ -7,7 +7,8 @@ public class LocksTest {
      */
     @Test
     void deadlockTest(){
-        Locks.deadlock();
+        Thread runner = new Thread(Locks::deadlock);
+        runner.start();
         try {
             Thread.sleep(1000L);
         } catch (InterruptedException ignored) {
@@ -26,12 +27,17 @@ public class LocksTest {
      */
     @Test
     void livelockTest(){
-        Locks.livelock();
-        try {
-            Thread.sleep(1000L);
-        } catch (InterruptedException ignored) {
-        }
+        Thread runner = new Thread(Locks::livelock);
+        runner.start();
+        long start = System.currentTimeMillis();
+        try{
+            long timeToWait = 2000;
+            runner.join(timeToWait);
+            long duration = System.currentTimeMillis() - start;
+            assert !(duration < timeToWait * 0.9);
+        } catch (InterruptedException ignored){
 
+        }
         for (Thread thread : Thread.getAllStackTraces().keySet()) {
             assert !thread.getState().equals(Thread.State.BLOCKED);
         }
