c1d12f3fcc6f71577c94f12fc5f176091e7f460c
Ivan <39248996+kLubidorka@users.noreply.github.com>
2020-11-27 10:27:04 +0400

    added comments to chart builder

--- Sem13/DB/src/main/java/DB/AirtransChartBuilder.java
+++ Sem13/DB/src/main/java/DB/AirtransChartBuilder.java
@@ -14,10 +14,10 @@ import org.jfree.data.category.DefaultCategoryDataset;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.util.List;
 
 /**
- * Creates charts representing results of QueryRunner
+ * Creates charts representing the results of some QueryRunner queries
  */
 public class AirtransChartBuilder {
     private final QueryRunner queryRunner;
@@ -28,7 +28,14 @@ public class AirtransChartBuilder {
         queryRunner = runner;
     }
 
-    private DefaultCategoryDataset fillDataset(ArrayList<String[]> data) {
+    /**
+     * Converts List of strings to JFreeChart dataset
+     * @param data List of String.
+     *             List[0] -- numeric values for Y axis
+     *             List[1] -- string values for X axis
+     * @return JFreeChart dataset
+     */
+    private DefaultCategoryDataset fillDataset(List<String[]> data) {
         final DefaultCategoryDataset dataset = new DefaultCategoryDataset();
         for (String[] row : data) {
             dataset.addValue(Double.parseDouble(row[1]), "default", row[0]);
@@ -36,6 +43,11 @@ public class AirtransChartBuilder {
         return dataset;
     }
 
+    /**
+     * Apply default settings to the chart and set up title
+     * @param barChart chart to set up
+     * @param title title
+     */
     private void setupChart(JFreeChart barChart, String title) {
         CategoryPlot plot = barChart.getCategoryPlot();
         CategoryAxis axis = plot.getDomainAxis();
@@ -52,8 +64,13 @@ public class AirtransChartBuilder {
         renderer.setBarPainter(new StandardBarPainter());
     }
 
+    /**
+     * Creates a chart, representing the number of cancelled flights as a function of month
+     * @param filepath where to store the chart
+     * @throws IOException if a problem while saving chart to the filesystem occurred
+     */
     public void query4CreateBarChart(String filepath) throws IOException {
-        ArrayList<String[]> data = queryRunner.getCancellationStatistics();
+        List<String[]> data = queryRunner.getCancellationStatistics();
         DefaultCategoryDataset dataset = fillDataset(data);
         String title = "Number of cancelled flights w.r.t month";
         String categoryAxis = "Month";
@@ -65,9 +82,15 @@ public class AirtransChartBuilder {
         ChartUtilities.saveChartAsPNG(new File(filepath), barChart, chartWidth, chartHeight);
     }
 
+    /**
+     * Creates two charts, representing the number of flights to (1st) and from (2nd) Moscow as a function of the weekday
+     * @param filepath1 where to store the first chart
+     * @param filepath2 where to store the second chart
+     * @throws IOException if a problem while saving chart to the filesystem occurred
+     */
     public void query5CreateBarCharts(String filepath1, String filepath2) throws IOException {
         for (int i = 0; i < 2; i++) {
-            ArrayList<String[]> data;
+            List<String[]> data;
             data = i == 0 ? queryRunner.getFlightsToMoscow() : queryRunner.getFlightsFromMoscow();
             DefaultCategoryDataset dataset = fillDataset(data);
             String title = i == 0 ? "Number of flights to Moscow" : "Number of flights from Moscow";
@@ -81,8 +104,15 @@ public class AirtransChartBuilder {
         }
     }
 
+    /**
+     * Creates a chart, representing the airway carrier's foregone earnings as a function of date
+     * @param filepath where to store the chart
+     * @param from String with the date corresponds to the start of the period. Format "dd.mm.yyyy"
+     * @param till String with the date corresponds to the end of the period. Format "dd.mm.yyyy"
+     * @throws IOException if a problem while saving chart to the filesystem occurred
+     */
     public void query6CreateBarCharts(String filepath, String from, String till) throws IOException {
-        ArrayList<String[]> data = queryRunner.cancelFlights(from, till);
+        List<String[]> data = queryRunner.cancelFlights(from, till);
         DefaultCategoryDataset dataset = fillDataset(data);
         String title = "Foregone earnings w.r.t date";
         String categoryAxis = "Date";
