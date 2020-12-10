78493d664f4533c41752849e8d2fb97b2a181de0
Ivan <39248996+kLubidorka@users.noreply.github.com>
2020-12-10 14:36:14 +0400

    added CAFE BABE

---
+++ Sem14/BytecodeWithoutMaven/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    private String field;
+
+    public String getField(){
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+}
+
