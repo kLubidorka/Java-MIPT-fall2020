fb80b49d188c7e9166dbadb19dde8ca114e6550e
Ivan <39248996+kLubidorka@users.noreply.github.com>
2020-12-10 14:04:34 +0400

    added dummy class for sem 14

---
+++ Sem14/Bytecode/src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    private String field;
+
+    public String getField(){
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+}
